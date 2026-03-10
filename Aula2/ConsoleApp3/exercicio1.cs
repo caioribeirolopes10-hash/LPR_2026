9ead2ece05e589085124ba20c22254206fe24297
guih_vb <caioribeirolopes10@gmail.com.brgit config --global user.name caioribeirolopes10-hash>
2026-03-10 13:45:29 -0300

    PrimeiroCommit

---
+++ Aula2/ConsoleApp3/exercicio1.cs
@@ -0,0 +1,4 @@
+﻿Console.WriteLine("Digite o valor de um raio de um círculo e eu direi a área do mesmo");
+float raio = Convert.ToInt32(Console.ReadLine());
+float pi = 3.14159f;
+Console.WriteLine("A área é :" + (raio * raio * pi));
