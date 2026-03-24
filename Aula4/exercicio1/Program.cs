a82706fb93f92a9049e6517705161255209bf235
guih_vb <caioribeirolopes10@gmail.com.brgit config --global user.name caioribeirolopes10-hash>
2026-03-24 09:48:32 -0300

    aula4

---
+++ Aula4/exercicio1/Program.cs
@@ -0,0 +1,13 @@
+﻿using System.Globalization;
+
+Console.WriteLine("Digite um número e direi se ele é par ou ímpar");
+int x = Convert.ToInt32(Console.ReadLine());
+
+if (x % 2 == 0)
+{
+    Console.WriteLine("Seu número é par");
+}
+else
+{
+    Console.WriteLine("Seu númro é ímpar");
+} 
